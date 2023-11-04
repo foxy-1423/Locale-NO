d01e8a61de0556f4ff7f96f48690e92155566c60
Krystof Holan <krystofholan@gmail.com>
2023-11-04 22:41:32 +0100

    Combined back to Json, not yet verified

--- PyhonCode/combine2.py
+++ PyhonCode/combine2.py
@@ -1,33 +1,66 @@
 import json
 
-def merge_json_files(file1_path, file2_path):
-    with open(file1_path, 'r', encoding='utf-8') as file1:
-        data1 = json.load(file1)
-
-    with open(file2_path, 'r', encoding='utf-8') as file2:
-        data2 = json.load(file2)
-
-    # Recursively update data1 with data2
-    def update_data(data1, data2):
-        if isinstance(data1, dict) and isinstance(data2, dict):
-            for key, value in data2.items():
-                if key in data1:
-                    data1[key] = update_data(data1[key], value)
+# Function to read the text file with extracted words and their numeric IDs
+def read_words_with_numeric_ids_from_text_file(text_file):
+    words_with_numeric_ids = {}
+    with open(text_file, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                parts = line.split(': ', 1)
+                if len(parts) == 2:
+                    numeric_id, word = parts
+                    words_with_numeric_ids[int(numeric_id)] = word
                 else:
-                    data1[key] = value
-            return data1
-        elif isinstance(data1, list) and isinstance(data2, list):
-            data1.extend(data2)
-            return data1
-        else:
-            return data2
+                    print(f"Skipping invalid line: {line}")
+    return words_with_numeric_ids
 
-    updated_data = update_data(data1, data2)
+# Function to save the recombined data to a JSON file with proper encoding
+def save_json_to_file(data, output_file):
+    with open(output_file, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
-    with open('merged_no_file.json', 'w', encoding='utf-8') as output_file:
-        json.dump(updated_data, output_file, indent=4, ensure_ascii=False)
+# Function to recombine the words with numeric IDs and the modified JSON data
+def recombine_data_with_numeric_ids(json_data, words_with_numeric_ids):
+    def replace_ids_with_words(data):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if isinstance(value, int):
+                    data[key] = words_with_numeric_ids.get(value, value)
+                else:
+                    replace_ids_with_words(value)
+        elif isinstance(data, list):
+            for index, item in enumerate(data):
+                if isinstance(item, int):
+                    data[index] = words_with_numeric_ids.get(item, item)
+                else:
+                    replace_ids_with_words(item)
+
+    copied_data = copy_json_data(json_data)  # Create a copy of the modified JSON data
+    replace_ids_with_words(copied_data)
+    return copied_data
+
+# Function to create a copy of the original JSON data
+def copy_json_data(data):
+    return json.loads(json.dumps(data))
 
 if __name__ == "__main__":
-    file1_path = 'en.json'  # Replace with the path to your first JSON file
-    file2_path = 'no.json'  # Replace with the path to your second JSON file
-    merge_json_files(file1_path, file2_path)
+    # Replace 'output.json' with the path to your JSON file with numeric IDs
+    json_file_with_ids = 'Temp_no.json'
+
+    # Replace 'output.txt' with the path to your text file with numeric IDs
+    text_file = 'Translated-no.txt'
+
+    # Replace 'combined_output.json' with the desired output JSON file name
+    combined_output_file = 'combined_output.json'
+
+    with open(json_file_with_ids, 'r', encoding='utf-8') as file:
+        json_data_with_ids = json.load(file)
+
+    words_with_numeric_ids = read_words_with_numeric_ids_from_text_file(text_file)
+    recombined_data = recombine_data_with_numeric_ids(json_data_with_ids, words_with_numeric_ids)
+
+    # Save the recombined data to the combined JSON file
+    save_json_to_file(recombined_data, combined_output_file)
+
+    print(f"Recombined data saved to {combined_output_file}")
